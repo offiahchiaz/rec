3ec3ba174694345bb48dda82637f1946792ea963
offiahchiaz <offiahchiaz@gmail.com>
2023-10-10 14:37:17 +0100

    Added Foundation directory

---
+++ foundations/src/App.js
@@ -0,0 +1,19 @@
+
+
+function App() {
+  let name = "mario"
+
+  const handleClick = () => {
+    name = 'luigi';
+    console.log(name)
+  }
+  
+  return (
+    <div className="App">
+      <h1>My name is {name}</h1>
+      <button onClick={handleClick}>Change name</button>
+    </div>
+  );
+}
+
+export default App;
